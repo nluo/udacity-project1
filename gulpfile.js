c93716b0b557e7d4999fa96262062162033d61f3
Nick Luo <nick.luo@outlook.com>
2016-01-11 22:35:40 +1000

    Added server task to gulp file, updated html

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 var gulp = require('gulp');
 var minifyCss = require('gulp-minify-css');
+var static = require('node-static');
+var fileServer = new static.Server('./');
+var http = require('http');
 
 gulp.task('styles', function () {
-  gulp.src('css/*.css')
-  .pipe(minifyCss())
-  .pipe(gulp.dest('./dist/'))
+    gulp.src('css/*.css')
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./dist/'))
 });
 
 gulp.task('watch', function () {
     gulp.watch('css/*.css', ['styles'])
 });
 
-gulp.task('default', ['styles', 'watch']);
+
+gulp.task('server', function () {
+    http.createServer(function (request, response) {
+        request.addListener('end', function() {
+            fileServer.serve(request, response);
+        }).resume();
+    }).listen(8080);
+});
+
+gulp.task('default', ['styles', 'server', 'watch']);
